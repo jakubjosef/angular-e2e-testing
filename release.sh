099dc9b3ebc0b5f666b680a229080abf60c870da
Jakub Josef <jjosef@ves.io>
2021-02-04 16:39:25 +0100

    updated release script

--- release.sh
+++ release.sh
@@ -3,6 +3,6 @@ echo "- Build docker image"
 docker build --no-cache -t volterrasystem/angular-e2e-testing .
 echo "- Build finished, pushing latest tag to volterrasystem"
 docker push volterrasystem/angular-e2e-testing
-echo "- Pushing node12 tag to volterrasystem"
-docker tag volterrasystem/angular-e2e-testing volterrasystem/angular-e2e-testing:node12
-docker push volterrasystem/angular-e2e-testing:node12
+echo "- Pushing node14 tag to volterrasystem"
+docker tag volterrasystem/angular-e2e-testing volterrasystem/angular-e2e-testing:node14
+docker push volterrasystem/angular-e2e-testing:node14
